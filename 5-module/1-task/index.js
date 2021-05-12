e6e2a9614dc73dd5256a4fb92cfe3c4dca0bf51a
ss <shambazov@gmail.com>
2021-05-12 10:40:53 +0200

    Первые два сделаны, тесты проходят.

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,11 @@
 function hideSelf() {
-  // ваш код...
+  const onClick = () => {
+    button.setAttribute('hidden', 'true');
+  };
+
+  const button = document.querySelector('.hide-self-button');
+
+  button.addEventListener('click', () => {
+    onClick();
+  });
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,18 @@
 function toggleText() {
-  // ваш код...
+  const currentText = document.getElementById('text');
+  const button = document.querySelector('.toggle-text-button');
+
+  const onClick = () => {
+    const state = (currentText.getAttribute('hidden'));
+    if (state === null) {
+      currentText.setAttribute('hidden', 'true');
+    } else {
+      currentText.removeAttribute('hidden');
+    }
+  };
+
+  button.addEventListener('click', () => {
+    onClick();
+  });
+
 }
