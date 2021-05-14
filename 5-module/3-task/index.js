7a9a357deb0ebc1a5a649477451e2b1351e672cd
ss <shambazov@gmail.com>
2021-05-14 15:17:22 +0200

    Черновой вариант, не завершено.

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  const carousel = document.querySelector('.carousel');
+  const width = carousel.offsetWidth;
+
+  const rightButton = document.querySelector('.carousel__arrow_right');
+  const leftButton = document.querySelector('.carousel__arrow_left');
+  const buttons = document.querySelector('.carousel__arrow');
+  const carouselInner = document.querySelector('.carousel__inner');
+
+  const onClickRight = () => {
+    carouselInner.style.transform = `translateX(-${width}px)`;
+  };
+
+  rightButton.addEventListener('click', () => {
+    console.log("A");
+    onClickRight();
+  });
+
+  const onClickLeft = () => {
+    carouselInner.style.transform = `translateX(${width}px)`;
+  };
+
+  leftButton.addEventListener('click', () => {
+    console.log("B");
+    onClickLeft();
+  });
+
 }
+
+// function hideSelf() {
+//   const onClick = () => {
+//     button.setAttribute('hidden', 'true');
+//   };
+//
+//   const button = document.querySelector('.hide-self-button');
+//
+//   button.addEventListener('click', () => {
+//     onClick();
+//   });
+// }
